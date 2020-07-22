8323d58eaab2b41c1318ae4aee529fc63c649b44
Mandora <trc19930224@aliyun.com>
2020-07-22 13:56:18 +0800

    Initial Commit

---
+++ SYNovelbbTests/SYNovelbbTests.swift
@@ -0,0 +1,34 @@
+//
+//  SYNovelbbTests.swift
+//  SYNovelbbTests
+//
+//  Created by Mandora on 2020/7/22.
+//  Copyright © 2020 Mandora. All rights reserved.
+//
+
+import XCTest
+@testable import SYNovelbb
+
+class SYNovelbbTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
