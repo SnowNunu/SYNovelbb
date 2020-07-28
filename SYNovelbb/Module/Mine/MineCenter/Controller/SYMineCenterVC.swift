28f50b4d984ba321026d49300660ed989506b738
Mandora <trc19930224@aliyun.com>
2020-07-28 18:30:08 +0800

    开始修改Mine模块

---
+++ SYNovelbb/Module/Mine/MineCenter/Controller/SYMineCenterVC.swift
@@ -0,0 +1,31 @@
+//
+//  SYMineCenterVC.swift
+//  SYNovelbb
+//
+//  Created by Mandora on 2020/7/28.
+//  Copyright © 2020 Mandora. All rights reserved.
+//
+
+import UIKit
+
+class SYMineCenterVC: SYBaseVC {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func setupUI() {
+        
+    }
+    
+    override func rxBind() {
+        
+    }
+
+}
